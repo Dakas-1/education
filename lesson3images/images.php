7402030933c9a22872f88fba07b89b1d4ab014cd
Dakas-1 <155234892+Dakas-1@users.noreply.github.com>
2024-01-21 23:14:37 +0300

    Delete lesson3images directory

--- lesson3images/images.php
+++
@@ -1,9 +0,0 @@
-<?php
-$images = [1 => '01.jpg', 2 => '02.jpg', 3 => '03.jpg', 4 => '04.jpg'];
-
-if (array_key_exists('id', $_GET)) {
-    echo '<img src="http://php1.local/lesson3images/images/' . $images[$_GET['id']] . '"</img>'; // <img src="images/03.img"></img>
-} else {
-    echo 'нихуя не найдено';
-}
-?>
