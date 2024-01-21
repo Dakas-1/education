9f90e6e347a81ee1d8b17e32ed4ae5381423a977
Dakas-1 <155234892+Dakas-1@users.noreply.github.com>
2024-01-21 23:14:00 +0300

    Delete lesson12 directory

--- lesson12/App/Models/Author.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/../Model.php';
-
-class Author extends Model
-{
-    public const TABLE = ' authors';
-    public $name;
-}
--- lesson12/article.php
+++
@@ -1,8 +0,0 @@
-<?php
-$template = __DIR__ . '/templates/article.php';
-require __DIR__ . '/App/Models/Article.php';
-require __DIR__ . '/App/Models/View.php';
-$id = $_GET['id'];
-$view = new View();
-$view->article = Article::findById($id);
-$view->display($template);
