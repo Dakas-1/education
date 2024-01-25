cc32adca17d8cc8c5e439de4c8d88435fadd63c3
Dakas <dakas9521@gmail.com>
2024-01-25 23:53:11 +0300

    delete isset in function, add isset in action, add type-hinting

--- lesson13/App/Controllers/Registration.php
+++ lesson13/App/Controllers/Registration.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controllers;
+
 use App\Models\Controller;
 
 require_once __DIR__ . '/../Models/Article.php';
@@ -14,22 +15,21 @@ class Registration extends Controller
 
         $template = __DIR__ . '/../../templates/registration.php';
         $this->view->display($template);
-        $this->registration();
-
-    }
-
-    protected function registration()
-    {
         if (isset($_POST['login']) && isset($_POST['auth_pass'])) {
             $login = $_POST['login'];
             $password = $_POST['auth_pass'];
-            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-            require_once __DIR__ . '/../Models/Db.php';
-            $Db = new \Db();
-            $sql = "INSERT INTO users (user, hash) VALUES (:user, :hash)";
-            $data = ['user' => $login, 'hash' => $hashed_password];
-            $Db->execute($sql, $data);
-            header("Location: ./login.php");
+            $this->registration($login, $password);
         }
+    }
+
+    protected function registration(string $login, string $password)
+    {
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        require_once __DIR__ . '/../Models/Db.php';
+        $Db = new \Db();
+        $sql = "INSERT INTO users (user, hash) VALUES (:user, :hash)";
+        $data = ['user' => $login, 'hash' => $hashed_password];
+        $Db->execute($sql, $data);
+        header("Location: ./login.php");
     }
 }
