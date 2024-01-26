53f6995cecd757236937a4163d33170a95c4076a
Dakas <dakas9521@gmail.com>
2024-01-26 15:44:42 +0300

    add aurhor

--- lesson13/App/Controllers/Admin.php
+++ lesson13/App/Controllers/Admin.php
@@ -4,7 +4,8 @@ namespace App\Controllers;
 use App\Models\Controller;
 
 require_once __DIR__ . '/../Models/Article.php';
-require_once __DIR__ . '/../Models/Controller.php';
+require_once __DIR__ . '/../Models/Author.php';
+require_once __DIR__ . '/../Controllers/Controller.php';
 
 class Admin extends Controller
 {
@@ -19,25 +20,30 @@ class Admin extends Controller
     }
     public function Crud()
     {
-        if (isset ($_POST['title']) && isset ($_POST['content']) && isset ($_GET['id']) === false) {
+        if (isset ($_POST['title']) && isset ($_POST['content']) && isset ($_POST['author']) && isset ($_GET['id']) === false) {
             $article = new \Article();
             $article->title = $_POST['title'];
             $article->content = $_POST['content'];
+            $author = new \Author();
+            $author->name = $_POST['author'];
             $article->save();
+            $author->save();
             header('Location: http://php1.local/lesson13/public/index.php/?ctrl=Admin');
         } elseif (isset($_GET['id'])) {
-            $result = \Article::findById($_GET['id']);
-            $article = null;
-            if (isset($result)) {
-                $article = $result;
-            }
+                $article = \Article::findById($_GET['id']);
+                if ($article) {
+                    $author = \Author::findById($article->author_id);
+                }
             if (isset ($_POST['title']) && isset ($_POST['content']) && isset ($_POST['delete']) === false) {
                 $article->title = $_POST['title'];
                 $article->content = $_POST['content'];
+                $author->name = $_POST['author'];
                 $article->save();
+                $author->save();
                 header('Location: http://php1.local/lesson13/public/index.php/?ctrl=Admin');
             }elseif (isset($_POST['delete'])){
                 $article->delete();
+                $author->delete();
                 header('Location: http://php1.local/lesson13/public/index.php/?ctrl=Admin');
             }
         } else {
