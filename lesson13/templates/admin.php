b04384bcce2406ac74d61f4a9a673d91d86a014f
Dakas <dakas9521@gmail.com>
2024-01-26 15:50:51 +0300

    add author

--- lesson13/templates/admin.php
+++ lesson13/templates/admin.php
@@ -1,3 +1,6 @@
+<?php
+/** @var View $this */
+?>
 <!doctype html>
 <html lang="ru">
 <head>
@@ -22,22 +25,28 @@ if (isset($this)) {
     <p><label for="field1">Редактор статьи:</label></p>
     <p><textarea name="title" id="field1"><?php
             if (isset($_GET['id']) && isset($this)) {
-                $result = Article::findById($_GET['id']);
-                echo $result->title;
+                $article = Article::findById($_GET['id']);
+                echo $article->title;
             }
             ?></textarea></p>
 
     <p><label for="field2">Содержание статьи:</label></p>
     <p><textarea name="content" id="field2"><?php
-            if (isset($_GET['id']) && isset($this)) {
-                $result = Article::findById($_GET['id']);
-                echo $result->content;
+            if (isset($_GET['id']) && isset($article->content)) {
+                echo $article->content;
+            }
+            ?></textarea></p>
+
+    <p><label for="field3">Автор статьи:</label></p>
+    <p><textarea name="author" id="field3"><?php
+            if (isset($_GET['id']) && isset($article->author_id)) {
+                echo $article->author->name;
             }
             ?></textarea></p>
 
     <p><input type="submit" name="push" value="Отправить"></p>
     <?php
-    if (isset($_GET['id']) && isset($this)) {
+    if (isset($_GET['id']) && isset($article)) {
         echo '<p><input type="submit" name="delete" value="Удалить"></p>';
     }
     ?>
