8b7b62fcea8bfe0f07146bba8db18a61b743560e
Dakas <dakas9521@gmail.com>
2024-01-31 00:31:36 +0300

    throw new NotFoundException();

--- lesson14/App/Controllers/Article.php
+++ lesson14/App/Controllers/Article.php
@@ -14,24 +14,16 @@ class Article extends Controller
 
     protected function action()
     {
-//        $id = $_GET['id'];
-//        $res = $this->view->article = \App\Models\Article::findById($id);
-//        if (!$res){
-//            throw new NotFoundException();
-//        }
-//        $template = __DIR__ . '/../../templates/article.php';
-//        $this->view->display($template);
-        //$id = $_GET['id'];
-        //$this->view->article = \App\Models\Article::findById($id);
-        //$template = __DIR__ . '/../../templates/article.php';
-        //$this->view->display($template);
-        $data = [
-            'title' => 'Заголовок',
-            'content' => 'Текст новости',
-            'author_id' => '1'
-        ];
-        $articles = new \App\Models\Article();
-        $this->view->article = $articles->fill($data);
-        var_dump($this->view->article);
+        $id = $_GET['id'];
+        $res = $this->view->article = \App\Models\Article::findById($id);
+        if (!$res){
+            throw new NotFoundException();
+        }
+        $template = __DIR__ . '/../../templates/article.php';
+        $this->view->display($template);
+        $id = $_GET['id'];
+        $this->view->article = \App\Models\Article::findById($id);
+        $template = __DIR__ . '/../../templates/article.php';
+        $this->view->display($template);
     }
 }
